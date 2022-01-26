63db1ea8f4f234e1fb41c73da544ca6c654ec89d
Ambre Gorias <ambre.gorias@gmail.com>
2022-01-25 21:08:54 -1000

    prefers-color-scheme

--- colorMode.js
+++ colorMode.js
@@ -1,22 +1,42 @@
 let darkToggle = document.getElementById('button-dark');
 let lightToggle = document.getElementById('button-light');
 
+function switchToDark() {
+  document.documentElement.setAttribute('color-mode','dark');
+  // localStorage.setItem("color-mode", "dark");
+
+}
+
+function switchToLight() {
+  document.documentElement.setAttribute('color-mode','light');
+  // localStorage.setItem("color-mode", "light");
+
+}
+
+
 function toggleColorMode(e) {
 
   // Switch to dark mode
   if (e.currentTarget.classList.contains('color-mode__btn--dark')) {
-    document.documentElement.setAttribute('color-mode','dark');
+    switchToDark();
+    return;
 
   // Switch to light mode
   } else {
-    document.documentElement.setAttribute('color-mode','light');
+    switchToLight();
+    return;
   }
 }
 
 // Toggle color when light or dark button is clicked
-
 let colorToggleButtons = document.querySelectorAll('.color-mode__btn');
 
 colorToggleButtons.forEach(button => {
   button.addEventListener("click", toggleColorMode);
 });
+
+
+// Check whether the user has set a preference for dark mode and automatically changes color mode
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  switchToDark();
+}
