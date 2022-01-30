f46dd84b3723cd12b3c026ca8f813663215b7319
Ambre Gorias <ambre.gorias@gmail.com>
2022-01-29 19:07:49 -1000

    prettier

--- javascript/colorMode.js
+++ javascript/colorMode.js
@@ -1,40 +1,32 @@
-  // If `prefers-color-scheme` is not supported, fall back to light mode.
-  if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
-    switchToLight();
-  } 
-  else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    switchToDark();
-  } 
-  else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-    switchToLight();
-  }
+// If `prefers-color-scheme` is not supported, fall back to light mode.
+if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
+  switchToLight();
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  switchToDark();
+} else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+  switchToLight();
+}
 
-
-
-let darkToggle = document.getElementById('button-dark');
-let lightToggle = document.getElementById('button-light');
+let darkToggle = document.getElementById("button-dark");
+let lightToggle = document.getElementById("button-light");
 
 function switchToDark() {
-  document.documentElement.setAttribute('color-mode','dark');
+  document.documentElement.setAttribute("color-mode", "dark");
   // localStorage.setItem("color-mode", "dark");
-
 }
 
 function switchToLight() {
-  document.documentElement.setAttribute('color-mode','light');
+  document.documentElement.setAttribute("color-mode", "light");
   // localStorage.setItem("color-mode", "light");
-
 }
 
-
 function toggleColorMode(e) {
-
   // Switch to dark mode
-  if (e.currentTarget.classList.contains('color-mode__btn--dark')) {
+  if (e.currentTarget.classList.contains("color-mode__btn--dark")) {
     switchToDark();
     return;
 
-  // Switch to light mode
+    // Switch to light mode
   } else {
     switchToLight();
     return;
@@ -42,8 +34,8 @@ function toggleColorMode(e) {
 }
 
 // Toggle color when light or dark button is clicked
-let colorToggleButtons = document.querySelectorAll('.color-mode__btn');
+let colorToggleButtons = document.querySelectorAll(".color-mode__btn");
 
-colorToggleButtons.forEach(button => {
+colorToggleButtons.forEach((button) => {
   button.addEventListener("click", toggleColorMode);
 });
--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 // Check whether data is filled or empty
 function checkData(data) {
-  if ((data == null) || (data == "")) {
+  if (data == null || data == "") {
     return false;
   }
   return true;
@@ -17,27 +17,38 @@ function getData(data, defaultText) {
 // Modify date format from YYYY-MM-DDTHH:MM:SSZ to " DD [Mon]th YYYY"
 // example : 2011-01-25T18:44:36Z --> "25 Jan 2011"
 function getFormattedDate(rawDate) {
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-  let day = rawDate.slice(8,10);
+  let day = rawDate.slice(8, 10);
   // Extract month, convert to number and return correponding month in months[]
-  let month = months[Number(rawDate.slice(5,7)) - 1];
-  let year = rawDate.slice(0,4);
+  let month = months[Number(rawDate.slice(5, 7)) - 1];
+  let year = rawDate.slice(0, 4);
 
   let formattedDate = " " + day + " " + month + " " + year;
   return formattedDate;
 }
-
 
 function fillUserInfo(userData) {
   //Default text when data was not filled by user
   const defaultBio = "This profile has no bio";
   const notAvailableText = "Not available";
 
-
   // Setting user profile pic
   const userPic = document.getElementById("userPic");
-  userPic.setAttribute('src',`${userData.avatar_url}`);
+  userPic.setAttribute("src", `${userData.avatar_url}`);
 
   // Setting user name and username
   let userNameElt = document.querySelector(".user-info__name");
@@ -55,10 +66,10 @@ function fillUserInfo(userData) {
   date.innerText = formattedDate;
 
   // Setting description
-  let description =document.querySelector('.user-info__description');
+  let description = document.querySelector(".user-info__description");
   let bio = getData(userData.bio, defaultBio);
   if (bio == defaultBio) {
-    description.classList.add('not-available');
+    description.classList.add("not-available");
   }
   description.innerText = bio;
 
@@ -74,103 +85,101 @@ function fillUserInfo(userData) {
 
   //Setting "where to find me" section
   // Location
-  let locationElt = document.getElementById('location');
+  let locationElt = document.getElementById("location");
   let location = getData(userData.location, notAvailableText);
   if (location == notAvailableText) {
-    locationElt.parentElement.classList.add('not-available');
+    locationElt.parentElement.classList.add("not-available");
   } else {
-    locationElt.parentElement.classList.remove('not-available');
+    locationElt.parentElement.classList.remove("not-available");
   }
   locationElt.innerText = location;
 
   // User website
-  const websiteElt = document.getElementById('website');
-  const websiteContainer = document.getElementById('websiteContainer');
+  const websiteElt = document.getElementById("website");
+  const websiteContainer = document.getElementById("websiteContainer");
   let website = getData(userData.blog, notAvailableText);
   console.log(website);
   if (website == notAvailableText) {
-    websiteContainer.classList.add('not-available');
+    websiteContainer.classList.add("not-available");
     let websiteData = document.createElement("p");
     websiteData.classList.add("user-infos__links__text");
-    websiteData.setAttribute('id','website');
+    websiteData.setAttribute("id", "website");
     websiteData.innerText = website;
     if (websiteElt != null) {
-      websiteContainer.replaceChild(websiteData,websiteElt);
+      websiteContainer.replaceChild(websiteData, websiteElt);
     } else {
       websiteContainer.append(websiteData);
     }
   } else {
-    websiteContainer.classList.remove('not-available');
+    websiteContainer.classList.remove("not-available");
     let websiteData = document.createElement("a");
     websiteData.classList.add("user-infos__links__text");
-    websiteData.setAttribute('id','website');
-    websiteData.setAttribute('href', website);
+    websiteData.setAttribute("id", "website");
+    websiteData.setAttribute("href", website);
     websiteData.innerText = website;
     if (websiteElt != null) {
-      websiteContainer.replaceChild(websiteData,websiteElt);
+      websiteContainer.replaceChild(websiteData, websiteElt);
     } else {
       websiteContainer.append(websiteData);
     }
   }
 
-
   // User twitter
-  const twitterElt = document.getElementById('twitter');
-  const twitterContainer = document.getElementById('twitterContainer');
+  const twitterElt = document.getElementById("twitter");
+  const twitterContainer = document.getElementById("twitterContainer");
   let twitter = getData(userData.twitter_username, notAvailableText);
   console.log(twitter);
   if (twitter == notAvailableText) {
-    twitterContainer.classList.add('not-available');
+    twitterContainer.classList.add("not-available");
     let twitterData = document.createElement("p");
     twitterData.classList.add("user-infos__links__text");
-    twitterData.setAttribute('id','twitter');
+    twitterData.setAttribute("id", "twitter");
     twitterData.innerText = twitter;
     if (twitterElt != null) {
-      twitterContainer.replaceChild(twitterData,twitterElt);
+      twitterContainer.replaceChild(twitterData, twitterElt);
     } else {
       twitterContainer.append(twitterData);
     }
   } else {
-    twitterContainer.classList.remove('not-available');
+    twitterContainer.classList.remove("not-available");
     let twitterData = document.createElement("a");
     twitterData.classList.add("user-infos__links__text");
-    twitterData.setAttribute('id','twitter');
-    twitterData.setAttribute('href', `https://twitter.com/${twitter}`);
+    twitterData.setAttribute("id", "twitter");
+    twitterData.setAttribute("href", `https://twitter.com/${twitter}`);
     twitterData.innerText = twitter;
     if (twitterElt != null) {
-      twitterContainer.replaceChild(twitterData,twitterElt);
+      twitterContainer.replaceChild(twitterData, twitterElt);
     } else {
       twitterContainer.append(twitterData);
     }
   }
 
-
   // User company
-  const companyElt = document.getElementById('company');
-  const companyContainer = document.getElementById('companyContainer');
+  const companyElt = document.getElementById("company");
+  const companyContainer = document.getElementById("companyContainer");
   let company = getData(userData.company, notAvailableText);
   console.log(company);
   if (company == notAvailableText) {
-    companyContainer.classList.add('not-available');
+    companyContainer.classList.add("not-available");
     let companyData = document.createElement("p");
     companyData.classList.add("user-infos__links__text");
-    companyData.setAttribute('id','company');
+    companyData.setAttribute("id", "company");
     companyData.innerText = company;
     if (companyElt != null) {
-      companyContainer.replaceChild(companyData,companyElt);
+      companyContainer.replaceChild(companyData, companyElt);
     } else {
       companyContainer.append(companyData);
     }
   } else {
     companyUrl = company.substring(1);
-    companyContainer.classList.remove('not-available');
+    companyContainer.classList.remove("not-available");
     let companyData = document.createElement("a");
     companyData.classList.add("user-infos__links__text");
-    companyData.setAttribute('id','company');
-    companyData.setAttribute('href', `https://github.com/${companyUrl}`);
+    companyData.setAttribute("id", "company");
+    companyData.setAttribute("href", `https://github.com/${companyUrl}`);
     companyData.innerText = company;
     if (companyElt != null) {
-      companyContainer.replaceChild(companyData,companyElt);
+      companyContainer.replaceChild(companyData, companyElt);
     } else {
       companyContainer.append(companyData);
     }
@@ -179,54 +188,50 @@ function fillUserInfo(userData) {
 
 // Fetch and display @octocat data by default
 fetch("https://api.github.com/users/octocat")
-  .then(function(res) {
+  .then(function (res) {
     if (res.ok) {
       return res.json();
     }
   })
-  .then(function(jsonUserData) {
+  .then(function (jsonUserData) {
     console.log(jsonUserData);
     fillUserInfo(jsonUserData);
   })
-  .catch(function(error){
+  .catch(function (error) {
     console.log(error);
-});
+  });
 
-let form = document.getElementById('form');
-let inputElt = document.getElementById('gitUsername');
+let form = document.getElementById("form");
+let inputElt = document.getElementById("gitUsername");
 let errorElt = document.querySelector(".error");
 
-
 // Fetch Github API with input username
-form
-  .addEventListener('submit', function(e){
-    e.preventDefault();
-    let user = inputElt.value;
-    fetch(`https://api.github.com/users/${user}`)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  // Display user data if user is found 
-  .then(function(jsonUserData) {
-    console.log(jsonUserData);
-    fillUserInfo(jsonUserData);
-  })
-  // Display error message if no user is found 
-  .catch(function(error) {
-    if (errorElt.classList.contains("hidden")) {
-    errorElt.classList.remove("hidden");
-  }
-    console.log(error);
-
-  });
-})
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let user = inputElt.value;
+  fetch(`https://api.github.com/users/${user}`)
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    // Display user data if user is found
+    .then(function (jsonUserData) {
+      console.log(jsonUserData);
+      fillUserInfo(jsonUserData);
+    })
+    // Display error message if no user is found
+    .catch(function (error) {
+      if (errorElt.classList.contains("hidden")) {
+        errorElt.classList.remove("hidden");
+      }
+      console.log(error);
+    });
+});
 
 // Hide error message when user clicks on form
-form
-  .addEventListener('click', function(e){
-    if (!errorElt.classList.contains("hidden")) {
-      errorElt.classList.add("hidden")
-    }
+form.addEventListener("click", function (e) {
+  if (!errorElt.classList.contains("hidden")) {
+    errorElt.classList.add("hidden");
+  }
 });
